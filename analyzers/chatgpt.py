9c0433f99da3da01568fb8e7488fdc6a78d51445
c <bali@gmail.com>
2023-04-26 11:39:10 +0000

    gpt-3.5-turbo is 90 percent cheaper with similar quality

--- analyzers/chatgpt.py
+++ analyzers/chatgpt.py
@@ -2,116 +2,41 @@ import openai
 import os
 from decouple import config
 
-
 openai.api_key = config("OPENAI_API_KEY")
 
-def summarize(text):
-  
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Human: Try to summarize the following text\n\n{text}\nAI:",
-    temperature=0.5,
-    max_tokens=250,
-    top_p=1,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=[" Human:", " AI:"]
-  )
+def chat_gpt(message, temperature=1.0, max_tokens=None, top_p=1.0, stop=None, presence_penalty=0.0, frequency_penalty=0.0):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}],
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p,
+        stop=stop,
+        presence_penalty=presence_penalty,
+        frequency_penalty=frequency_penalty
+    )
+    return response.choices[0].message['content']
 
-  return response.choices[0].text
+def summarize(text):
+    return chat_gpt(f"Try to summarize the following text: {text}", temperature=0.5, max_tokens=250, stop=[" Human:", " AI:"])
 
 def get_labels(text):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Human: what label in following text\n\n{text}\nAI:",
-    temperature=0.2,
-    max_tokens=100,
-    top_p=1,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=[" Human:", " AI:"]
-  )
+    return chat_gpt(f"what label in following text: {text}", temperature=0.2, max_tokens=100, stop=[" Human:", " AI:"])
 
-  return response.choices[0].text
-  
 def get_namespace(text):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Human: abstract namespace in following text\n\n{text}\nAI:",
-    temperature=0,
-    max_tokens=100,
-    top_p=1,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=[" Human:", " AI:"]
-  )
-
-  return response.choices[0].text
+    return chat_gpt(f"abstract namespace in following text: {text}", temperature=0, max_tokens=100, stop=[" Human:", " AI:"])
 
 def create_k8s_yaml(text, type, additional_text=""):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Convert this text to a k8s {type} yaml {additional_text}:\n\n{text}",
-    temperature=0,
-    max_tokens=500,
-    top_p=1,
-    frequency_penalty=0.5,
-    presence_penalty=0.0
-  )
-  # print(response.choices[0].text)
-  return response.choices[0].text
+    return chat_gpt(f"Convert this text to a k8s {type} yaml {additional_text}: {text}", temperature=0, max_tokens=500, frequency_penalty=0.5)
 
 def create_kubectl_cmd(text):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Convert this text to a kubectl command:\n\n{text}",
-    temperature=0,
-    max_tokens=250,
-    top_p=1,
-    frequency_penalty=0.5,
-    presence_penalty=0.0
-  )
-
-  return response.choices[0].text
+    return chat_gpt(f"Convert this text to a kubectl command: {text}", temperature=0, max_tokens=250, frequency_penalty=0.5)
 
 def create_kubectl_cmd_formated(text, format):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Convert this text to a kubectl command:\n\n{text}\n\n{format}",
-    temperature=0,
-    max_tokens=250,
-    top_p=1,
-    frequency_penalty=0.2,
-    presence_penalty=0.0
-  )
-
-  # print(response.choices[0].text)
-  return response.choices[0].text
+    return chat_gpt(f"Convert this text to a kubectl command: {text}\n\n{format}", temperature=0, max_tokens=250, frequency_penalty=0.2)
 
 def create_json(text):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Convert this text to json:\n\n{text}",
-    temperature=0,
-    max_tokens=500,
-    top_p=1,
-    frequency_penalty=0.2,
-    presence_penalty=0.0
-  )
-
-  # print(response.choices[0].text)
-  return response.choices[0].text
+    return chat_gpt(f"Convert this text to json: {text}", temperature=0, max_tokens=500, frequency_penalty=0.2)
 
 def rewrite_text(text):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Rewrite this text:\n\n{text}",
-    temperature=0,
-    max_tokens=500,
-    top_p=1,
-    frequency_penalty=0.2,
-    presence_penalty=0.0
-  )
-
-  # print(response.choices[0].text)
-  return response.choices[0].text
+    return chat_gpt(f"Rewrite this text: {text}", temperature=0, max_tokens=500, frequency_penalty=0.2)
